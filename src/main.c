04c224a501c2f9b94c734a18e1b8332af05ab79b
LukaMaruna <lukamaruna03@protonmail.com>
2025-05-15 09:42:03 +0200

    Added splashart and main menu
    Only the visual aspect of the menu has been added.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "menu.h"
 #include "game.h"
 #include "snake.h"
+#include <windows.h>
+#include <stdio.h>
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
     int game_running = 1;
 
     while (game_running) {
