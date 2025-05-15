56c0277b44df624ca4a2511d973caec974a69d9a
Urkelaa <uros.radojevic03@gmail.com>
2025-05-15 15:29:40 +0200

    Game_logic

--- src/game.c
+++ src/game.c
@@ -1,3 +1,70 @@
 #include "game.h"
+#define WIDTH 20
+#define HEIGHT 20
 
-void intialize_game(){}
+typedef struct {
+    int x, y;
+} Koord;
+
+typedef struct{
+    Koord body[100];    //Zmija je niz 2D koordinata
+    int length;         //Duzina zmije
+    char direction;     //Trenutna direkcija u kojoj ide (W, A, S, D)
+} Snake;
+
+Koord food;             
+Snake snake;            //globalna promenljiva koja predstavlja zmiju
+int game_over = 0;      //flag za kraj
+
+void intialize_game(){
+    snake.length = 1;
+    snake.body[0].x = WIDTH/2;
+    snake.body[0].y = HEIGHT/2;
+    snake.direction = 'D';
+    generate_food();
+}                           //Funkcija postavlja igru na pocetak
+
+void move_snake(){
+    for(int i = snake.length-1; i>0; i--){
+        snake.body[i] = snake.body[i-1];    //petlja koja ispomera telo zmije bez glave za jedno mesto u napred;  Posmatramo da je glava nulti indeks u telu
+    }
+
+    switch(snake.direction){
+        case 'W': snake.body[0].y++;
+            break;
+        case 'A': snake.body[0].x--;
+            break;
+        case 'S': snake.body[0].y--;
+            break;
+        case 'D': snake.body[0].x++;
+            break;
+    }                                       //pomeranje glave zmije u odnosu na unos
+}   
+
+void check_collision(){
+    Koord glava = snake.body[0];
+
+    if(glava.x < 0 || glava.x >= WIDTH || glava.y < 0 || glava.y >= HEIGHT)
+        game_over = 1;
+
+    for(int i = 1; i<snake.direction-1; i++){
+        if(glava.x == snake.body[i].x && glava.y == snake.body[i].y)
+            game_over = 1;
+    }
+}
+
+void generate_food(){
+    int a = 1;
+
+    while(a){
+        food.x = rand() % WIDTH;
+        food.y = rand() % HEIGHT;
+        a = 0;
+        for(int i = 1; i<snake.length-1; i++){
+            if(food.x == snake.body[i].x || food.y == snake.body[i].y){
+                a = 1;
+                break;
+            }
+        }
+    }
+}
--- src/game.h
+++ src/game.h
@@ -1,6 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
+#include "time.h"
+#include "stdlib.h"
 
 void intialize_game();
+void move_snake();
+void check_collision();
+void generate_food();
+void update_game_state();
 
 #endif
